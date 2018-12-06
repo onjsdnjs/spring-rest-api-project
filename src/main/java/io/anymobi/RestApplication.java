d1acd633fc1c58a256c6a0960af734e9f88c8f10
onjsdnjs <onjsdnjs@gmail.com>
2018-12-06 18:02:35 +0900

    second commit

--- src/main/java/io/anymobi/RestApplication.java
+++ src/main/java/io/anymobi/RestApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestApplication.class, args);
     }
 }
